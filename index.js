7e49f3e908cbaeab0c8d4077ed27eec611b173d8
Camila Vergara <camilavergara021@gmail.com>
2024-02-06 14:15:26 +0100

    funcionalidad desencriptar texto

--- index.js
+++ index.js
@@ -25,4 +25,30 @@ function encriptar() {
 
 }
 
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi,"i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi,"o")
+    .replace(/ufat/gi,"u");
+
+    if(texto.lenght != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";
+    }else {
+        muñeco.src = "./img/muñeco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar  o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
+
 
