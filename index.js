3396f43e184a8c553a7f89bb5c8ceb1cd9612d2c
Camila Vergara <camilavergara021@gmail.com>
2024-02-06 13:58:31 +0100

    diseño de la aplicacion con  css y la funcionalidad encriptar texto

---
+++ index.js
@@ -0,0 +1,28 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+    
+    if (texto.lenght != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/encriptado.jpg";
+    } else {
+        muñeco.src = "./img/muñeco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar  o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+
+}
+
+
